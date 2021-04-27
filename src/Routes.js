8232d7bbd03bbb7ee6811b48189c620126960efd
Eddington524 <iamsam8687@gmail.com>
2021-04-27 21:44:37 +0900

    Modify:스타일 컴포넌트 리뷰 수정 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './Pages/Login/index';
+import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/index';
 import JobList from './Pages/JobList/index';
 import JobDetails from './Pages/JobDetails/index';
