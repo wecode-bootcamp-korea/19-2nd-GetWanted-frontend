d2e668c74855939d26248471b9fea094b80bc7e7
hello-dodohee <73160509+hello-dodohee@users.noreply.github.com>
2021-04-29 03:01:47 +0900

    Modify: 코드 리팩토링

---
+++ src/Pages/JobList/Components/JobItems.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import * as styled from './JobItem.style';
+import { FaHeart } from 'react-icons/fa';
+
+const JobItems = ({ name, company, image, area, heartCount }) => {
+  const [liked, setLiked] = useState(false);
+
+  const handletoggleHeart = () => {
+    setLiked(!liked);
+  };
+
+  return (
+    <styled.JobItem>
+      <styled.JobItemImg>
+        <img src={image} alt="기업사진"></img>
+        <styled.JobHeartBtn liked={liked} onClick={handletoggleHeart}>
+          <FaHeart />
+          <styled.HeartCount>
+            {liked ? heartCount + 1 : heartCount}
+          </styled.HeartCount>
+        </styled.JobHeartBtn>
+      </styled.JobItemImg>
+      <styled.JobItemContent>
+        <styled.ItemTask>{name}</styled.ItemTask>
+        <styled.ItemCompany>{company}</styled.ItemCompany>
+        <styled.ItemLocation>{area}</styled.ItemLocation>
+        <styled.ItemBonus>채용보상금 1,000,000원</styled.ItemBonus>
+      </styled.JobItemContent>
+    </styled.JobItem>
+  );
+};
+
+export default JobItems;
